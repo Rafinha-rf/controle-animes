42291431942f19236ca1f496f4d752819dc4e7af
Rafinha-rf <rafinha951026@gmail.com>
2024-02-27 00:55:32 -0300

    fazendo alguns ajustes sugestão de anime na pesquisa na tela de adicionar um novo anime

--- resources/js/anime_suggestions.js
+++ resources/js/anime_suggestions.js
@@ -1,27 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Manipulador de evento para clicar em um item da lista de sugestões
-    $(document).on('click', '#suggestions li', function() {
-        var selectedValue = $(this).text();
+    $(document).on('click', '#suggestions li', function () {
+        let selectedValue = $(this).text();
         $('#nome').val(selectedValue);
         $('#suggestions').empty(); // Limpa a lista de sugestões após selecionar
     });
 
-    $('#nome').on('input', function() {
-        var query = $(this).val();
+    let xhr; // Variável para armazenar a solicitação AJAX
+
+    $('#nome').on('input', function () {
+        let query = $(this).val();
         if (query.length > 2) {
-            $.ajax({
+            // Cancela a solicitação AJAX anterior se houver uma
+            if (xhr && xhr.readyState !== 4) {
+                xhr.abort();
+            }
+            xhr = $.ajax({
                 url: 'https://kitsu.io/api/edge/anime',
                 method: 'GET',
-                data: { filter: { text: query } },
-                success: function(response) {
+                data: {
+                    filter: {
+                        text: query
+                    }
+                },
+                success: function (response) {
                     $('#suggestions').empty();
-                    response.data.forEach(function(anime) {
+                    response.data.forEach(function (anime) {
                         if (anime.attributes && anime.attributes.titles && anime.attributes.titles.en_jp) {
                             $('#suggestions').append('<li class="list-group-item">' + anime.attributes.titles.en_jp + '</li>');
                         }
                     });
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr, status, error) {
                     console.error(error);
                 }
             });
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,4 @@ const mix = require('laravel-mix');
 
 mix
     .sass('resources/css/app.scss', 'public/css')
+    .js('resources/js/anime_suggestions.js', 'public/js')
